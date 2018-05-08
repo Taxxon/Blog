3782fb389eada0344b6dd7ff3f797fc989991b06
Taxxon <te15.emil.kack@umea.nti.se>
2018-05-08 14:29:25 +0200

    Uppdate
    Nu funkar delete knappen :3

--- POST/POST.php
+++ POST/POST.php
@@ -1,113 +1,101 @@
+<?php
+	try{
+		$dbh = new PDO("mysql:host=localhost;dbname=post;charset=utf8", "root", "", array(
+			PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,));
+	} catch(PDOException $pe){
+		echo $pe->getMessage();
+	}
+?>
+
 <!DOCTYPE html>
-
 <html>
-
 <head>
-
 	<title>POST</title>
-
 	 <link rel="stylesheet" type="text/css" href="style.css">
-
 	 <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
-
 </head>
-
 <body>
 
 	<div class="formdiv">
 		<form action="POST.php" method="post">
 			<label for="title">Titel</label> 
 			<input type="text" id="title" name="title" size="29" placeholder="Title" required>
-			
 			<label for="content">Text</label>
 			<textarea name="content" class="content" placeholder="Content" required rows="20"></textarea>
-
 			<lable for="image">Bild</lable>
 			<input type="text" id="image" name="image" size="29" placeholder="Image" required>
 			<input type="submit" name="submit" value="Submit">
 		</form>
 	</div>
 
-	<form action="POST.php" method="post">
 		<table class="table">
-			<tr>
-				<th>Id</th>
-				<th>Userid</th>
-				<th>Title</th>
-				<th>Content</th>
-				<th>Image</th>
-				<th>Date</th>
-				<th> </th>
-				<th> </th>
-			</tr>
+			<thead>
+				<tr>
+					<th>Id</th>
+					<th>Userid</th>
+					<th>Title</th>
+					<th>Content</th>
+					<th>Image</th>
+					<th>Date</th>
+					<th> </th>
+					<th> </th>
+				</tr>
+			</thead>
+			<tbody>
 			<?php
-				$dbh = new PDO("mysql:host=localhost;dbname=post;charset=utf8" , 
-					"root",
-					"");
-				$sql = "select * from posts";
+				$sql = "SELECT * FROM posts";
 				$stmt = $dbh->prepare($sql);
 				$stmt->execute();
 				$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-				foreach ($rows as $key => $value) {
-					
-					echo "<tr>";
-					echo "<td>" . $value["id"] . "</td>";
-					echo "<td>" . $value["userid"] . "</td>";
-					echo "<td>" . $value["title"] . "</td>";
-					echo "<td>" . $value["content"] . "</td>";
-					echo "<td>" . $value["image"] . "</td>";
-					echo "<td>" . date("Y-m-d", strtotime($value["date"])) . "</td>";
-					echo "<input type=\"hidden\" name=\"postid1\" value=" . $value["id"] .">";
-					echo "<td><input type=\"submit\" name=\"delete\" value=\"Delete\"></td>";
-					echo "<input type=\"hidden\" name=\"postid2\" value=" . $value["id"] .">";
-					echo "<td><input type=\"submit\" name=\"edit\" value=\"Edit\"</td>";
-					echo "</tr>";
+				foreach ($rows as $key => $value){
+					?>
+					<form action="POST.php" method="post">
+						<tr>
+							<td><?php echo $value['id'] ?></td>
+							<td><?php echo $value['userid'] ?></td>
+							<td><?php echo $value['title'] ?></td>
+							<td><?php echo $value['content'] ?></td>
+							<td><?php echo $value['image'] ?></td>
+							<td><?php echo date("Y-m-d", strtotime($value['date'])) ?></td>
+							<input type="hidden" name="id" value=<?php echo $value['id'];?>>
+							<td><input type="submit" name="delete" value="Delete"></td>
+							<td><input type="submit" name="edit" value="Edit"></td>
+						</tr>
+						</form>
+				<?php
 				}
-
-			?>
+				?>
+			</tbody>
 		</table>
-	</form>
 
 <?php
-
-	$dbh = new PDO("mysql:host=localhost;dbname=post;charset=utf8", "root", "");
-	
 	if(isset($_POST['submit'])){
-
-		$Title = filter_input(INPUT_POST, 'title', FILTER_SANITIZE_SPECIAL_CHARS);
-		$Content = filter_input(INPUT_POST, 'content', FILTER_SANITIZE_SPECIAL_CHARS);
-		$Image = filter_input(INPUT_POST, 'image', FILTER_SANITIZE_SPECIAL_CHARS);
-
-
-		try{
-			$sql = "INSERT INTO posts(userid,title,content,image) VALUES(1 , ' " . $_POST['title'] . " ', ' " . $_POST['content'] . " ', ' " . $_POST['image'] . " ' )";
-			$stmt = $dbh->prepare($sql);
-			$stmt->execute();
-		} catch(PDDExeption $e){
-			$e->getMessage();
-		}
+		$title = filter_input(INPUT_POST, 'title', FILTER_SANITIZE_SPECIAL_CHARS);
+		$content = filter_input(INPUT_POST, 'content', FILTER_SANITIZE_SPECIAL_CHARS);
+		$image = filter_input(INPUT_POST, 'image', FILTER_SANITIZE_SPECIAL_CHARS);
+		$stmt = $dbh->prepare("INSERT INTO posts(title, content, image) VALUES (:title, :content, :image)");
+		$stmt->bindParam(':title', $title,  PDO::PARAM_STR);
+	    $stmt->bindParam(':content', $content,  PDO::PARAM_STR);
+	   	$stmt->bindParam(':image', $image,  PDO::PARAM_STR);
+	   	$stmt->execute();
 
 		Header("Location: ../POST/POST.php");
 		exit();
-}
-	
-	if(isset($_POST['delete'])){
-		
-		try {
-			$sql = "DELETE FROM posts WHERE id= " . $_POST['postid1'] . "";
-			$dbh->exec($sql);
-		} catch(PDOException $e) {
-			$e->getMessage();		
-		}
-
+	}elseif(isset($_POST['delete'])){
+		//var_dump($_POST['id']);
+		$id = filter_input(INPUT_POST, "id", FILTER_VALIDATE_INT);
+		$sql = "DELETE FROM posts WHERE id = :id";
+		$stmt = $dbh->prepare($sql);	
+		$stmt->bindParam(':id', $id, PDO::PARAM_INT);
+		$stmt->execute();
+		echo $_POST['delete'];
+		var_dump($id);
 		Header("Location: ../POST/POST.php");
 		exit();
-}
+	}elseif(isset($_POST['edit'])){
 
-	if(isset($_POST['edit'])){
-
-		$sql = "SELECT * FROM posts WHERE id= " . $_POST['postid2'] . "";
+		$sql = "SELECT * FROM posts WHERE id= " . $_POST['postid'] . "";
 		
 		echo $sql;
 
@@ -134,9 +122,7 @@
  		echo "</div>";
 
  		exit();
-	}
-
-	if(isset($_POST['editpost'])){
+	}elseif(isset($_POST['editpost'])){
 
 		try{
 			$sql = "UPDATE posts SET title ='" . $_POST['title'] . "', content ='" . $_POST['content'] . "', image ='" . $_POST['image'] . "' WHERE id =" . $_POST['id'] . "";
@@ -149,9 +135,6 @@
 		//Header("Location: ../POST/POST.php");
 		//exit();
 	}
-
 ?>
-
 </body>
-
-</html>
+</html>	
